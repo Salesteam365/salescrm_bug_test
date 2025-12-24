452e8777af6ef35f66c4bba37c8283d89143e692
CodeAnt AI <support@codeant.ai>
2025-12-24 07:31:45 +0000

    changed files

--- application/views/assets/js/sticky.js
+++ application/views/assets/js/sticky.js
@@ -2,8 +2,8 @@
 "use strict";
 (() => {
   window.addEventListener('scroll', stickyFn);
-  var navbar = document.getElementById("sidebar");
-  var sticky = navbar.offsetTop;
+  let navbar = document.getElementById("sidebar");
+  let sticky = navbar.offsetTop;
   function stickyFn() {
     if (window.scrollY >= 75) {
       navbar.classList.add("sticky-pin")
