3cbf9761d9404326a9245e4d8eb34e5a118fa76c
CodeAnt AI <support@codeant.ai>
2025-12-26 04:48:51 +0000

    changed files

--- application/views/assets/libs/moment/moment.js
+++ application/views/assets/libs/moment/moment.js
@@ -974,7 +974,7 @@
         isPM : function (input) {
             // IE8 Quirks Mode & IE7 Standards Mode do not allow accessing strings like arrays
             // Using charAt should be more compatible.
-            return ((input + '').toLowerCase().charAt(0) === 'p');
+            return ((input + '').toLowerCase().startsWith('p'));
         },
 
         _meridiemParse : /[ap]\.?m?\.?/i,
