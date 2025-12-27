65e00cb10f5ee46f0cacbeaf7877c1b291ebf777
CodeAnt AI <support@codeant.ai>
2025-12-27 06:14:57 +0000

    changed files

--- application/views/assets/js/mail-settings.js
+++ application/views/assets/js/mail-settings.js
@@ -2,9 +2,9 @@
   'use strict';
 
   let loadFile = function (event) {
-      var reader = new FileReader();
+      let reader = new FileReader();
       reader.onload = function () {
-          var output = document.getElementById('profile-img');
+          let output = document.getElementById('profile-img');
           if (event.target.files[0].type.match('image.*')) {
               output.src = reader.result;
           } else {
@@ -40,9 +40,9 @@
   ProfileChange.addEventListener('change', loadFile);
    /* Start::Choices JS */
    document.addEventListener("DOMContentLoaded", function () {
-    var genericExamples = document.querySelectorAll("[data-trigger]");
+    let genericExamples = document.querySelectorAll("[data-trigger]");
     for (let i = 0; i < genericExamples.length; ++i) {
-    var element = genericExamples[i];
+    let element = genericExamples[i];
     new Choices(element, {
         allowHTML: true,
 
